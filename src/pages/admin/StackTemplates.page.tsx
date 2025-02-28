5b7585c8b471e367b6178256c11a36f30859dbf8
fm-labs <flowmotionlabs@gmail.com>
2025-02-28 09:51:00 +0100

    Added error handling

--- src/pages/admin/StackTemplates.page.tsx
+++ src/pages/admin/StackTemplates.page.tsx
@@ -8,12 +8,23 @@ import Toolbar from '@mui/material/Toolbar'
 import Heading from '../../elements/Heading.tsx'
 import BasicDialogButton from '../../elements/Dialog/BasicDialogButton.tsx'
 import AddTemplateForm from '../../components/templates/AddTemplateForm.tsx'
+import { toast } from 'react-toastify'
 
 const StackTemplatesPage = () => {
   const templates = useLoaderData() as any[]
   const [selectedTemplateName, setSelectedTemplateName] = React.useState('')
   const [templateData, setTemplateData] = React.useState([])
   const api = useEnvApi()
+
+  // sort templateDate by template_id
+  const sortedTemplates = React.useMemo(() => {
+    if (!templates) {
+      return []
+    }
+    return templates.sort((a: any, b: any) => {
+      return a.template_id - b.template_id
+    })
+  }, [templates])
 
   React.useEffect(() => {
     if (!selectedTemplateName) {
@@ -25,7 +36,8 @@ const StackTemplatesPage = () => {
       .then((data) => {
         setTemplateData(data)
       })
-      .catch(() => {
+      .catch((err) => {
+        toast.error('Failed to fetch template data: ' + err?.response?.data?.error)
         setTemplateData([])
       })
   }, [selectedTemplateName])
@@ -48,7 +60,7 @@ const StackTemplatesPage = () => {
       <div>
         <select onChange={(e) => setSelectedTemplateName(e.target.value)}>
           <option value=''>Select a template</option>
-          {templates.map((template) => (
+          {sortedTemplates.map((template: any) => (
             <option key={template.template_id} value={template.template_id}>
               {template.label}
             </option>
