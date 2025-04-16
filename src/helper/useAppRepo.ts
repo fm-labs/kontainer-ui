ff9c27991ead95933e7512b48570836dca40f7cb
fm-labs <flowmotionlabs@gmail.com>
2025-04-16 20:27:56 +0200

    Changed appRepo scope to docker context

--- src/helper/useAppRepo.ts
+++ src/helper/useAppRepo.ts
@@ -1,15 +1,17 @@
-import appRepo from '../lib/appRepo.ts'
 import React from 'react'
+import appRepo from '../lib/appRepo.ts'
+import { useDockerContext } from '~/helper/useDockerContext.tsx'
 import { useEnvironment } from '~/helper/useEnvironmentContext.tsx'
 
 export const useAppRepo = () => {
   const { environment } = useEnvironment()
+  const { dockerHost } = useDockerContext()
 
   return React.useMemo(() => {
-    if (!environment) {
+    if (!dockerHost) {
       throw new Error('Failed to init app repo: environment not found')
     }
 
-    return appRepo(environment.id)
-  }, [environment])
+    return appRepo(environment.id + '_' + dockerHost.id)
+  }, [dockerHost])
 }
